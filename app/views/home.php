7000e36c62c665d38009231f9e3e0e30b3ecb063
Tim Rodger <tim.rodger@gmail.com>
2013-10-26 11:53:47 +0100

    Adds a home page

---
+++ app/views/home.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Message queue</title>
+</head>
+<body>
+    <div class="content">A basic message queue.</div>
+    <div>View messages <a href="/messages/">here</a></div>
+</body>
+</html>
+
